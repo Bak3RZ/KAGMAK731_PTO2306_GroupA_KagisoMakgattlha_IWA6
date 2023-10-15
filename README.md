a9fedd3c3cde0a3c1106f736acc2fed01079273b
Bak3RZ <101631291+Bak3RZ@users.noreply.github.com>
2023-10-15 19:24:32 +0200

    IWA 6 Challenge 2
    IWA 6 Challenge 2

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# KAGMAK731_PTO2306_GroupA_KagisoMakgattlha_IWA6
- Challenge 1
+# KAGMAK731_PTO2306_GroupA_KagisoMakgatlha_IWA6.2
+ Challenge2
--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const primaryPhone = 'O748105141'
-const secondaryPhone = '0219131568'
+const rent = 400;
+let tax = "8%"; 
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 00; 
+const minuteOfDay = 00; 
 
-const primaryValid = typeof primaryPhone === 'string' && /^\d+$/.test(primaryPhone)
-const secondaryValid = typeof secondaryPhone === 'string' && /^\d+$/.test(secondaryPhone)
-
-console.log('Primary phone is valid numerical string:', primaryValid)
-console.log('Secondary phone is valid numerical string:', secondaryValid )
+if (+hourOfDay === 0 && +minuteOfDay === 0 && hourOfDay !== undefined && minuteOfDay !== undefined) {
+    let taxAsDecimal = parseFloat(tax) / 100;
+    const startingAfterTax = salary - taxAsDecimal;
+	const balance = startingAfterTax - transport - food - rent;
+}
+	
+console.log("R" + balance.toFixed(2))
